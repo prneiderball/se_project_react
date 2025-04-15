fc40ca465a71fd4f638692230e10ffeaeee2a242
P.R. Neider-Ball <phillrballn@icloud.com>
2025-04-15 13:08:43 -0500

    update project

--- wtwr/src/components/App/App.jsx
+++ wtwr/src/components/App/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import './Header.css'
+import './Footer.css'
 import Header from './Header'
-import Main from './Main'
+import Footer from './Footer'
 
 function App() {
   return (
     <div>
       <Header />
-      <Main />
+      <Footer />
     </div>
   )
 }
---
+++ wtwr/src/components/App/WeatherCard.jsx
@@ -0,0 +1,5 @@
+function WeatherCard() {
+    return <></>;
+}
+
+  export default WeatherCard;
