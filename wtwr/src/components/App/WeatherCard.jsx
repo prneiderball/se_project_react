44642ba124f37e94db1f9ff7f735f460e2fb556e
P.R. Neider-Ball <phillrballn@icloud.com>
2025-04-18 21:50:21 -0500

    update weathercard

--- wtwr/src/components/App/WeatherCard.jsx
+++ wtwr/src/components/App/WeatherCard.jsx
@@ -1,11 +1,11 @@
-import Weathercard from '../../assets/Group 130.svg'
+import Weathercardimage from '../../assets/weathercard.svg'
 import './WeatherCard.css';
 
 function WeatherCard() {
     return (
         <section className="weather__card">
-            <p className="weather__text">75 F</p>
-            <img src={Weathercard} alt="Weather Card" className="weather__card_img" />
+            <p className="weather__text">75 &deg; F</p>
+            <img src={Weathercardimage} alt="Weather Card" className="weather__card_img" />
         </section>
     );
     }
