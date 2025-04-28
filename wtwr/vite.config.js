3af4c6defa7da67ae0f0adb788a24533ae3b2e7a
prneiderball <phillrballn@icloud.com>
2025-04-27 19:58:55 -0500

    Update vite.config.js

--- wtwr/vite.config.js
+++ wtwr/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/se_project_react/wtwr",
+  base: "/se_project_react/wtwr/",
   plugins: [react()],
   server: {
     port: 3000
