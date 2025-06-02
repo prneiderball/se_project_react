23a9c994753e0d86b85298dd54154de7b33cf26c
P.R. Neider-Ball <phillrballn@icloud.com>
2025-06-02 15:23:01 -0500

    Update ClothesSection.jsx
    remov3d unused import

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,5 +1,4 @@
 import "./ClothesSection.css";
-import { defaultClothingItems } from "../../utils/constants.js";
 import ItemCard from "../ItemCard/ItemCard.jsx";
 
 function ClothesSection({ onCardClick, onAdd, clothingItems }) {
