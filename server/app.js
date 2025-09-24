a8bff8b4000861bde71a1fcfb3c49e8ebbaa3312
P.R. Neider-Ball <phillrballn@icloud.com>
2025-09-24 16:36:57 -0500

    updated

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
-const { PORT = 3001 } = process.env;
+const { PORT } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
