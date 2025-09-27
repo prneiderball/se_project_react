b19433d1c27f36fb9fa53faf956d2fe17eda0ebb
P.R. Neider-Ball <phillrballn@icloud.com>
2025-09-27 12:38:11 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -22,16 +22,11 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 
