cb7dc8cc1afd9934f707021b88c5ce1c26896161
P.R. Neider-Ball <phillrballn@icloud.com>
2025-09-30 09:07:08 -0500

    Update app.js
    added dynamic function to check origin

--- server/app.js
+++ server/app.js
@@ -15,18 +15,27 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+  // White list of allowed origins
 const allowedOrigins = [
   "http://localhost:3000",
   "https://prnbwtwr.twilightparadox.com",
 ];
 
+//CORS config: Validates incoming origin dynamically
 app.use(
   cors({
-    origin: allowedOrigins,
-    credentials: true,
+    //checks if incoming question is in allowedOrigin whitelist
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      //if true, allow
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
   })
 );
-
 
 app.use(express.json());
 
