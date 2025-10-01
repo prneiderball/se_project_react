183e7880c020cf35a4912ecf3cadb5f1061e0ca8
P.R. Neider-Ball <phillrballn@icloud.com>
2025-09-30 19:19:30 -0500

    Update app.js
    added crash test

--- server/app.js
+++ server/app.js
@@ -41,6 +41,12 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
 app.use("/", routes);
 
 app.use(errorLogger);
