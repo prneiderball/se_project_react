3339cca9e84cea9f3adffb66353af1a6c367883d
P.R. Neider-Ball <phillrballn@icloud.com>
2025-10-12 19:09:25 -0500

    renamed dir

--- se_project_express/routes/users.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { getCurrentUser, updateUserProfile } = require("../controllers/users");
-const { validateUserUpdate } = require("../middlewares/validation");
-
-router.get("/me", getCurrentUser);
-
-router.patch("/me", validateUserUpdate, updateUserProfile);
-
-module.exports = router;
