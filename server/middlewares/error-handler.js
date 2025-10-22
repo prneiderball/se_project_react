cde97b351b2b778b44bbea089a4de36137859966
P.R. Neider-Ball <phillrballn@icloud.com>
2025-10-22 10:55:47 -0500

    Update error-handler.js

--- server/middlewares/error-handler.js
+++ server/middlewares/error-handler.js
@@ -1,5 +1,3 @@
-const { logger } = require("express-winston");
-
 function errorHandler(err, req, res, next) {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
